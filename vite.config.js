b1b4212dfed3f40acb3e5006abfc876c9f2991f3
himanshu10rawat <hr14638@gmail.com>
2025-01-08 18:05:20 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/facebook-clone/", // Replace 'facebook-clone' with your repository name
-});
+  base: mode === "production" ? "/facebook-clone/" : "/", // Conditional base URL
+}));
