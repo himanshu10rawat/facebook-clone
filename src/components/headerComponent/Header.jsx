a62ce477be4677a7a557e7f4239c080a45b4ee24
himanshu10rawat <hr14638@gmail.com>
2025-01-10 21:09:56 +0530

    added search functionalty in header

--- src/components/headerComponent/Header.jsx
+++ src/components/headerComponent/Header.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router";
 import { FaFacebook, FaFacebookMessenger } from "react-icons/fa";
-import { MdOutlineOndemandVideo } from "react-icons/md";
-import { IoMdNotifications, IoIosSearch } from "react-icons/io";
+import { MdKeyboardBackspace, MdOutlineOndemandVideo } from "react-icons/md";
+import { IoMdNotifications, IoIosSearch, IoMdClose } from "react-icons/io";
 import { CgMenuGridR } from "react-icons/cg";
 import { BiHomeAlt } from "react-icons/bi";
 import { AiOutlineShop } from "react-icons/ai";
@@ -14,34 +14,177 @@ import { usePostContext } from "../../context/postContext";
 import HeaderModal from "../headerModalComponent/HeaderModal";
 
 const Header = () => {
+  const [headModal, setHeadModal] = useState(false);
   const { state } = usePostContext();
   const loginUser = state.users.find(
     (singleUser) => singleUser.userId === state.user.userId
   );
 
-  const [headModal, setHeadModal] = useState(false);
+  const [searchBarActive, setSearchBarActive] = useState(false);
+
+  const [searchListStyle, setSearchListStyle] = useState({
+    borderRadius: "0 0 1rem 1rem",
+    position: "absolute",
+    left: "0",
+    paddingInline: "1rem",
+    top: "50%",
+    transform: "translateY(-50%)",
+  });
+
+  const [searchUsersList, setSearchUsersList] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchChange = (event) => {
+    const searchInput = event.target.value;
+    setSearchValue(searchInput);
+
+    const searchUsers = state.users.filter((user) =>
+      user.firstName
+        .toLowerCase()
+        .startsWith(searchInput.length !== 0 && searchInput.toLowerCase())
+    );
+
+    setSearchUsersList(searchUsers);
+  };
+
+  const editSearchList = () => {
+    const searchUsers = state.users.filter((user) =>
+      user.firstName
+        .toLowerCase()
+        .startsWith(searchValue.length !== 0 && searchValue.toLowerCase())
+    );
+    setSearchUsersList(searchUsers);
+  };
+
+  useEffect(() => {
+    if (searchBarActive) {
+      setSearchListStyle({
+        ...searchListStyle,
+        boxShadow: "0 10px 5px 5px rgba(221, 221, 221, 0.5)",
+        backgroundColor: "#ffffff",
+        paddingBottom: "0.5rem",
+        top: "0",
+        transform: "unset",
+      });
+    } else {
+      setSearchListStyle({
+        ...searchListStyle,
+        boxShadow: "none",
+        backgroundColor: "transparent",
+        paddingBottom: "0",
+        top: "50%",
+        transform: "translateY(-50%)",
+      });
+    }
+  }, [searchBarActive]);
+
   return (
     <>
       <header className={style["header-section"]}>
-        <div className={style["left-side"]}>
-          <Link to={"/"}>
-            <FaFacebook />
-          </Link>
-          <div
-            role="button"
-            tabIndex={0}
-            aria-label="Search Fakebook"
-            className={style["search-button"]}
-          >
-            <IoIosSearch />
-            <input
-              type="search"
-              name="search"
-              id="search"
-              placeholder="Search Fakebook"
-              aria-label="Search option on facebook"
-            />
+        <div style={searchListStyle}>
+          <div className={style["left-side"]}>
+            {searchBarActive ? (
+              <div
+                role="button"
+                aria-label="close search bar"
+                tabIndex={0}
+                className={style["back-button"]}
+                onClick={() => {
+                  setSearchBarActive(false);
+                  setSearchUsersList([]);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === " " || e.key === "Enter") {
+                    setSearchBarActive(false);
+                  }
+                }}
+              >
+                <MdKeyboardBackspace />
+              </div>
+            ) : (
+              <Link to={"/"}>
+                <FaFacebook />
+              </Link>
+            )}
+
+            <div
+              onClick={() => {
+                setSearchBarActive(true);
+                if (searchValue.length !== 0) {
+                  editSearchList();
+                }
+              }}
+              onKeyDown={(event) => {
+                if (event.key === " " || event.key === "Enter") {
+                  setSearchBarActive(true);
+                  if (searchValue.length !== 0) {
+                    editSearchList();
+                  }
+                }
+              }}
+              role="button"
+              tabIndex={0}
+              aria-label="Search Fakebook"
+              className={`${style["search-button"]} ${style["setWidth"]}`}
+            >
+              {!searchBarActive && <IoIosSearch />}
+              <input
+                onChange={handleSearchChange}
+                value={searchValue}
+                type="search"
+                name="search"
+                id="search"
+                placeholder="Search Fakebook"
+                aria-label="Search option on facebook"
+              />
+            </div>
           </div>
+          {searchBarActive && (
+            <div className={style["search-list"]}>
+              {searchUsersList.length === 0 ? (
+                <div className={style["no-recent-searches"]}>
+                  No recent searches
+                </div>
+              ) : (
+                searchUsersList.map((searchUser) => {
+                  return (
+                    <Link
+                      to={`/${searchUser.userId}`}
+                      className={style["search-user"]}
+                      key={searchUser.userId}
+                      onClick={() => {
+                        setSearchBarActive(false);
+                        setSearchValue("");
+                        setSearchUsersList([]);
+                      }}
+                    >
+                      <div className={style["user-profile-pic"]}>
+                        {searchUser.profilePic ? (
+                          <img
+                            src={searchUser.profilePic}
+                            alt={
+                              searchUser.firstName +
+                              " " +
+                              searchUser.lastName +
+                              "profile pic"
+                            }
+                          />
+                        ) : (
+                          <FaUserCircle />
+                        )}
+                      </div>
+                      <div className={style["user-name"]}>
+                        {searchUser.firstName + " " + searchUser.lastName}
+                      </div>
+                      <div className={style["remove-search-user"]}>
+                        <IoMdClose />
+                      </div>
+                    </Link>
+                  );
+                })
+              )}
+            </div>
+          )}
         </div>
         <nav className={style["navbar-items"]}>
           <ul>
